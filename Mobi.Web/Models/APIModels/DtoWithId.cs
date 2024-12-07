a0862a55618a7430214f07083d5ad456eca209a2
ilyaspatel92 <ilupatel6@gmail.com>
2024-12-08 00:03:28 +0530

    Update the API RESPONSE

---
+++ Mobi.Web/Models/APIModels/DtoWithId.cs
@@ -0,0 +1,12 @@
+﻿namespace Mobi.Web.Models.APIModels
+{
+    public abstract class DtoWithId 
+    {
+        /// <summary>
+        /// Gets or sets the dto object identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        public int LanguageId { get; set; }
+    }
+}
---
+++ Mobi.Web/Models/APIModels/ResponseModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Mobi.Web.Models.APIModels
+{
+    public class ResponseModel<T>
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; }
+
+        public T Data { get; set; }
+
+        public Exception Exception { get; set; }
+
+
+        public ResponseModel()
+        {
+            Success = true;
+        }
+    }
+}
