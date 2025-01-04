66c083591cfb362fc2e0112471cff8f2f58df99f
Sharukh Shaikh <sharukhshaik1169@gmail.com>
2025-01-04 20:12:57 +0530

    - MobileManage list added

--- Mobi.Web/Controllers/MobileManageController.cs
+++ Mobi.Web/Controllers/MobileManageController.cs
@@ -1,13 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Mobi.Service.Employees;
+using Mobi.Web.Factories.Employees;
+using Mobi.Web.Models;
+using Mobi.Web.Models.Employees;
 
 namespace Mobi.Web.Controllers
 {
     public class MobileManageController : BasePublicController
     {
-        [HttpGet]
-        public IActionResult MobileManage()
+
+        private readonly IEmployeeService _employeeService;
+        private readonly IEmployeeFactory _employeeFactory;
+
+        public MobileManageController(IEmployeeFactory employeeFactory, IEmployeeService employeeService)
         {
-            return View();
+            _employeeFactory = employeeFactory;
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public IActionResult MobileManage(string name, int? id, int page = 1, int pageSize = 5)
+        {
+            // Pass query string values to the view
+            ViewData["Name"] = name;
+            ViewData["Id"] = id?.ToString();
+
+            // Retrieve all employees
+            var query = _employeeService.GetAllEmployees();
+
+            // Apply filters if the parameters are provided
+            if (!string.IsNullOrEmpty(name))
+            {
+                query = query.Where(e => e.NameEng.Contains(name, StringComparison.OrdinalIgnoreCase) || e.NameArabic.Contains(name, StringComparison.OrdinalIgnoreCase));
+            }
+
+            if (id.HasValue)
+            {
+                query = query.Where(e => e.Id == id.Value);
+            }
+
+            // Pagination
+            var totalItems = query.Count();
+            var items = query.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            // Convert to ViewModels and pass to the view
+            var employeeViewModels = _employeeFactory.PrepareEmployeeViewModels(items);
+
+            var model = new PaginatedList<EmployeeModel>
+            {
+                Items = employeeViewModels.ToList(),
+                TotalItems = totalItems,
+                CurrentPage = page,
+                PageSize = pageSize
+            };
+
+            return View(model);
         }
     }
 }
