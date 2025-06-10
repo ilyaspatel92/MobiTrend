904ed752cdf5796cde8230d487762c9da85e6e22
Sharukh Shaikh <sharukhshaik1169@gmail.com>
2025-06-11 00:15:24 +0530

    #forgot password flow template added

---
+++ Mobi.Web/Models/Employees/ResetPasswordModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Mobi.Web.Models.Employees
+{
+    public class ResetPasswordModel
+    {
+        public string Token { get; set; }
+        public string Email { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
